b9ca0d15a986a5c56452a797cb4561b289017971
Pedro Seruca <pedroseruca@gmail.com>
2021-02-23 22:51:57 +0000

    Add content on ViewModel Template

---
+++ MVVM Templates/ViewModel.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,29 @@
+protocol ___FILEBASENAMEASIDENTIFIER___Protocol {
+    
+}
+
+final class ___FILEBASENAMEASIDENTIFIER___ {
+    // MARK: - Inner types
+
+    // MARK: - Private Properties
+
+    // MARK: - Public Properties
+
+    // MARK: - Lifecycle
+
+    init() {
+        
+    }
+}
+
+// MARK: - Private Methods
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+}
+
+// MARK: - Protocol Conformance
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Protocol {
+    
+}
